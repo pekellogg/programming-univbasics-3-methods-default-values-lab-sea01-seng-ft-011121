7b97a2237113bbdbb60b3c33c5b9d6a8fdd2a901
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-28 03:52:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,8 +7,6 @@ def meal_choice(veg1,veg2,protein = "tofu")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("Red pepper","Onion")
-
 # step 2
 def meal_choice(veg1, veg2, protein = 'tofu')
   puts "What a nutritious meal!"
